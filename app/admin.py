c733f98c0e57c554ed90803432175987ef9d8e70
EwenQuim <ewen.quimerch@gmail.com>
2020-09-30 15:19:17 +0200

    model: ingredient

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Recette
+from .models import Recette, Ingredient
 
 # Register your models here.
 admin.site.register(Recette)
+admin.site.register(Ingredient)
--- app/models.py
+++ app/models.py
@@ -2,10 +2,17 @@ from django.db import models
 
 # Create your models here.
 class Recette(models.Model):
-    id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=100)
     short = models.CharField(max_length=150, default="Description rapide et alléchante!")
     description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+    vegetarian = models.BooleanField(default=False)
+    recettes = models.ManyToManyField(Recette, related_name='recettes', blank=True)
 
     def __str__(self):
         return self.name
