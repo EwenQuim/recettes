7640e215fc70695a16c672607ff5a23f116328f8
EwenQuim <ewen.quimerch@gmail.com>
2020-10-01 00:15:06 +0200

    feat: clearer admin

--- app/admin.py
+++ app/admin.py
@@ -2,5 +2,16 @@ from django.contrib import admin
 from .models import Recette, Ingredient
 
 # Register your models here.
-admin.site.register(Recette)
 admin.site.register(Ingredient)
+
+
+class RecetteIngredientInline(admin.TabularInline):
+    model = Ingredient.recettes.through # the query goes through an intermediate table.
+    extra = 0
+    verbose_name = "Ingrédient"
+    verbose_name_plural = "Ingrédients"
+
+@admin.register(Recette)
+class RecetteAdmin(admin.ModelAdmin):
+    inlines = [RecetteIngredientInline,]
+
