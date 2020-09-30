1fa57da2760b4516c9b36ba3adcac8652e429908
EwenQuim <ewen.quimerch@gmail.com>
2020-10-01 00:51:06 +0200

    modified models to add ingredient dosage

--- app/models.py
+++ app/models.py
@@ -12,7 +12,21 @@ class Recette(models.Model):
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
     vegetarian = models.BooleanField(default=False)
-    recettes = models.ManyToManyField(Recette, related_name='recettes', blank=True)
+    recettes = models.ManyToManyField(Recette, related_name='recettes', blank=True, through='Dosage')
 
     def __str__(self):
         return self.name
+
+class Dosage(models.Model):
+    recette = models.ForeignKey(Recette, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quantite = models.IntegerField()
+
+    class Unites(models.TextChoices):
+        g = 'g', 'grammes'
+        mL = 'mL', 'millilitres'
+        cas = 'cas', 'cuillère à soupe'
+        cac = 'cac', 'cuillère à cafe'
+        p = 'pincee', 'pincée'
+
+    unite = models.CharField(max_length=20, choices=Unites.choices, default=Unites.g)
