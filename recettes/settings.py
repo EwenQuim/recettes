f3b479e333739b2abd5d539a4266bdd382480c27
EwenQuim <ewen.quimerch@gmail.com>
2020-10-02 12:17:18 +0200

    fix: imported os

--- recettes/settings.py
+++ recettes/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 from .local_settings import secret_key, DEBUG_
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
