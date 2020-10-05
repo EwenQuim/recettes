2c8a4483df076bbfaf42c93fbd363ac51bcd1b70
Ewen <46993939+EwenQuim@users.noreply.github.com>
2020-10-05 23:43:52 +0200

    server: updating efficiently and clearly (#21)

--- update.sh
+++ update.sh
@@ -1,9 +1,28 @@
 #!/bin/zsh
 
+echo "GIT - Pulling source code"
+git pull
+
+echo "PYTHON ENV - Activating virtualenv"
 source ./env/bin/activate
+
+echo "PYTHON ENV - Installing packages"
+pip install -r requirements.txt
+
+echo "DJANGO - Migrating Databases"
 python manage.py makemigrations
 python manage.py migrate
+
+echo "DJANGO - Collecting static files"
 python manage.py collectstatic --no-input
+
+echo "PYTHON ENV - Exiting virtualenv"
 deactivate
+
+echo "SERVER - Reload Daemons"
 sudo systemctl daemon-reload
+
+echo "SERVER - Restarting gunicorn"
 sudo systemctl restart gunicorn
+
+echo "Updating successful!"
