61cc4dfb0ba6d4a974106d69f5ae6ae30c5eb5c9
Ewen <46993939+EwenQuim@users.noreply.github.com>
2020-10-06 14:52:56 +0200

    👀 views: added ingredients list and other information
    Closes #13 #14 #15

--- app/views.py
+++ app/views.py
@@ -1,20 +1,34 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from .models import Recette
+from .models import Recette, Dosage, Ingredient
 
 # Create your views here.
 def index(request):
-    context = { "foo": "Fbedjsfnjkqfs" }
-    return render(request, 'app/index.html', context)
+    context = {"foo": "Fbedjsfnjkqfs"}
+    return render(request, "app/index.html", context)
+
 
 def listing(request):
     context = {}
     context["recettes"] = Recette.objects.all()
-    return render(request, 'app/listing.html', context)
+    return render(request, "app/listing.html", context)
+
 
 def detail(request, recette_id):
+    # Getting recipe
     recette = get_object_or_404(Recette, pk=recette_id)
-    return render(request, 'app/details.html', {'recette': recette})
+
+    # Getting ingredients
+    ingredients = []
+    for d in Dosage.objects.filter(recette_id=recette.id):
+        for i in Ingredient.objects.filter(id=d.ingredient_id):
+            ingredients.append(i)
+    context = {
+        "recette": recette,
+        "ingredients": ingredients
+    }
+    return render(request, "app/details.html", context)
+
 
 def search(request):
     return HttpResponse("ET ok")
