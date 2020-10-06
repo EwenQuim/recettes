2447a4cebef6b0c0bb335bb8e01f13a54a42373d
EwenQuim <ewen.quimerch@gmail.com>
2020-10-06 09:47:16 +0200

    feat: Clearer update

--- update.sh
+++ update.sh
@@ -3,29 +3,29 @@
 bold=$(tput bold)
 normal=$(tput sgr0)
 
-echo "${bold}GIT - Pulling source code${normal}"
+echo "$\n{bold}→ GIT - Pulling source code${normal}"
 git pull
 
-echo "${bold}PYTHON ENV - Activating virtualenv${normal}"
+echo "$\n{bold}→ PYTHON ENV - Activating virtualenv${normal}"
 source ./env/bin/activate
 
-echo "${bold}PYTHON ENV - Installing packages${normal}"
+echo "$\n{bold}→ PYTHON ENV - Installing packages${normal}"
 pip install -r requirements.txt
 
-echo "${bold}DJANGO - Migrating Databases${normal}"
+echo "$\n{bold}→ DJANGO - Migrating Databases${normal}"
 python manage.py makemigrations
 python manage.py migrate
 
-echo "${bold}DJANGO - Collecting static files${normal}"
+echo "$\n{bold}→ DJANGO - Collecting static files${normal}"
 python manage.py collectstatic --no-input
 
-echo "${bold}PYTHON ENV - Exiting virtualenv${normal}"
+echo "$\n{bold}→ PYTHON ENV - Exiting virtualenv${normal}"
 deactivate
 
-echo "${bold}SERVER - Reload Daemons${normal}"
+echo "$\n{bold}→ SERVER - Reload Daemons${normal}"
 sudo systemctl daemon-reload
 
-echo "${bold}SERVER - Restarting gunicorn${normal}"
+echo "$\n{bold}→ SERVER - Restarting gunicorn${normal}"
 sudo systemctl restart gunicorn
 
-echo "${bold}Updating successful!${normal}"
+echo "$\n{bold}→ Updating successful!${normal}"
