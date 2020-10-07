1d04dec80917193bccd71cf52fddafe139e491d1
EwenQuim <ewen.quimerch@gmail.com>
2020-10-07 20:53:36 +0200

    feat: testing model functions

--- app/tests.py
+++ app/tests.py
@@ -4,19 +4,38 @@ from app.models import Recette, Ingredient, Dosage
 class RecetteTestCase(TestCase):
 
     def setUp(self):
+        # Recettes
         instructions = "- Faire ceci \
                         - Faire cela"
-        Recette.objects.create(name="Pates Pesto", description="La recette des pâtes pesto!", instructions=instructions, active=True)
-        
-        Ingredient.objects.create(name="Pates", categorie="feculent")
-        Ingredient.objects.create(name="Pesto", categorie="sauce")
+        pp = Recette.objects.create(name="Pates Pesto", description="La recette des pâtes pesto!", instructions=instructions, active=True)
+
+        bolo = Recette.objects.create(name="Pates Bolo", description="Les pâtes bolo c'est mieux!", instructions=instructions, active=True)
+
+        # Ingredients
+        pates = Ingredient.objects.create(name="Pates", categorie="feculent")
+        pesto = Ingredient.objects.create(name="Pesto", categorie="sauce")
+
+        hache = Ingredient.objects.create(name="Viande Hachée", categorie="viande")
+        tomate = Ingredient.objects.create(name="Tomate", categorie="legume")
+
+        # Dosages
+        d = Dosage.objects.create(recette=pp, ingredient=pates, quantite=200, unite='g')
+        e = Dosage.objects.create(recette=pp, ingredient=pesto, quantite=20, unite='mL')
+
+        f = Dosage.objects.create(recette=bolo, ingredient=hache, quantite=200, unite='g')
+        g = Dosage.objects.create(recette=bolo, ingredient=tomate, quantite=200, unite='g')
+
+
 
 
     def test_get_object(self):
         pates_pesto = Recette.objects.get(name="Pates Pesto")
         self.assertEqual(pates_pesto.description, "La recette des pâtes pesto!")
 
-    # def test_if_is_vegetarian_from_list(self):
-    #     pates_pesto = Recette.objects.get(name="Pates Pesto")
-    #     self.assertEqual(pates_pesto.veggie(), True)
+    def test_if_is_vegetarian_from_list(self):
+        pates_pesto = Recette.objects.get(name="Pates Pesto")
+        self.assertEqual(pates_pesto.veggie(), True)
+
+        pates_bolo = Recette.objects.get(name="Pates Bolo")
+        self.assertEqual(pates_bolo.veggie(), False)
 
