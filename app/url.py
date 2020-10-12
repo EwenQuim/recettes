1ca92d65ff89150905281cace8f57fe950babba6
Ewen <46993939+EwenQuim@users.noreply.github.com>
2020-10-12 14:57:21 +0200

    ✨ feat: suggestion system draft (#48)
    * feat: suggestion system draft
    
    * fix: accessibility issue

--- app/url.py
+++ app/url.py
@@ -6,7 +6,10 @@ app_name = 'app'
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('a-propos', views.about, name="about"),
     path('recettes/<int:recette_id>/', views.detail, name="detail"),
     path('recettes/search/', views.search, name="recherche"),
     path('recettes/', views.listing, name="recettes"),
+    path('suggestion/', views.suggestion, name="suggestion"),
+    path('suggestion/<int:monAM>-<int:monPM>-<int:tueAM>-<int:tuePM>-<int:wedAM>-<int:wedPM>-<int:thuAM>-<int:thuPM>-<int:friAM>-<int:friPM>-<int:satAM>-<int:satPM>-<int:sunAM>-<int:sunPM>', views.suggestion, name="suggestion"),
 ]
