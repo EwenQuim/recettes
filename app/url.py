a13541fb7e0d09c64ed6375f19bc3a198544d9a0
EwenQuim <ewen.quimerch@gmail.com>
2020-10-13 11:11:45 +0200

    format: black, pylint, pre-commit hook

--- app/url.py
+++ app/url.py
@@ -1,15 +1,19 @@
+"""
+Take an url and calls the right view to process it
+"""
+
 from django.urls import path
 
-from . import views # import views so we can use them in urls.
+from . import views  # import views so we can use them in urls.
 
-app_name = 'app'
+app_name = "app"
 
 urlpatterns = [
-    path('', views.index, name="index"),
-    path('a-propos', views.about, name="about"),
-    path('recettes/<int:recette_id>/', views.detail, name="detail"),
-    path('recettes/search/', views.search, name="recherche"),
-    path('recettes/', views.listing, name="recettes"),
-    path('suggestion/', views.suggestion, name="suggestion"),
-    path('suggestion/<int:monAM>-<int:monPM>-<int:tueAM>-<int:tuePM>-<int:wedAM>-<int:wedPM>-<int:thuAM>-<int:thuPM>-<int:friAM>-<int:friPM>-<int:satAM>-<int:satPM>-<int:sunAM>-<int:sunPM>', views.suggestion, name="suggestion"),
+    path("", views.index, name="index"),
+    path("a-propos", views.about, name="about"),
+    path("recettes/<int:recette_id>/", views.detail, name="detail"),
+    path("recettes/search/", views.search, name="recherche"),
+    path("recettes/", views.listing, name="recettes"),
+    path("suggestion/", views.suggestion, name="suggestion"),
+    path("suggestion/<slug:slug>", views.suggestion, name="suggestion"),
 ]
--- recettes/settings.py
+++ recettes/settings.py
@@ -10,9 +10,19 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-from pathlib import Path
-from .local_settings import secret_key, DEBUG_, CSRF_COOKIE_SECURE_, SECURE_HSTS_INCLUDE_SUBDOMAINS_, SECURE_HSTS_PRELOAD_, SECURE_HSTS_SECONDS_, SECURE_SSL_REDIRECT_, SESSION_COOKIE_SECURE_
 import os
+from pathlib import Path
+
+from .local_settings import (
+    CSRF_COOKIE_SECURE_,
+    DEBUG_,
+    SECURE_HSTS_INCLUDE_SUBDOMAINS_,
+    SECURE_HSTS_PRELOAD_,
+    SECURE_HSTS_SECONDS_,
+    SECURE_SSL_REDIRECT_,
+    SESSION_COOKIE_SECURE_,
+    secret_key,
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,60 +37,60 @@ SECRET_KEY = secret_key
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = DEBUG_
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'recettes.quimerch.com']
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "recettes.quimerch.com"]
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'app.apps.AppConfig',
-    'markdown_deux',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "app.apps.AppConfig",
+    "markdown_deux",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'recettes.urls'
+ROOT_URLCONF = "recettes.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'recettes.wsgi.application'
+WSGI_APPLICATION = "recettes.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
@@ -90,16 +100,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -107,9 +117,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -121,8 +131,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT=os.path.join(BASE_DIR, 'static/')
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 # Deploy
 SECURE_HSTS_INCLUDE_SUBDOMAINS = SECURE_HSTS_INCLUDE_SUBDOMAINS_
